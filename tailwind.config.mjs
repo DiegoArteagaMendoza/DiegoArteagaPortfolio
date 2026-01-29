049cfa3a7c7ee7989b10284d58ca89167b3d3f49
diegoarteagamendoza <daarteagamendoza@gmail.com>
2026-01-29 17:30:31 -0300

    Primer Commit Portafolio

---
+++ tailwind.config.mjs
@@ -0,0 +1,14 @@
+export default {
+    // En v4 el content se detecta solo, está bien dejarlo comentado
+    theme: {
+        extend: {
+            colors: {
+                primary: '#38b6ff',
+                second: '#FF8138',
+                third: '#98bde6'
+                // Elimina accent y dark si no tienen color todavía
+            }
+        },
+    },
+    plugins: []
+}
